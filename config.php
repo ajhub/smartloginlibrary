734050b575faf1acd97cc95e6b40e09fb4b8117a
suprit <sethi_sprt@rediff.com>
2013-03-09 02:19:16 +0530

    pushing config.php and qrlogin.php in smartloginlibrary

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+	$SqlHost='localhost';
+	$SqlUser='root';
+	$SqlPass='suprit12';
+	$SqlDBname='smartlogin';
+	$SqlCon=mysql_connect($SqlHost,$SqlUser,$SqlPass,$SqlDBname);
+	$con=mysql_connect($SqlHost,$SqlUser,$SqlPass);
+	if(!$con)
+	{
+		die("Connection Error");
+	}
+	$db=mysql_select_db($SqlDBname,$con);
+	if(!$db)
+		die("Error in db delection..");
+	$ico_loc='<link rel="shortcut icon"  href="/favicon.ico">';
+	$randomdb='randomdb';
+	$randomdb_random='random';
+	$randomdb_user='username';
+?>
