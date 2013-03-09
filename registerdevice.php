06147d8c3d6e1084666ee37c705dd8f1c949f5cf
unknown <ajinkya.salve01@gmail.com>
2013-03-09 16:25:45 +0530

    Finished coding registerdevice.php , users can register their devices through this script

---
+++ registerdevice.php
@@ -0,0 +1,34 @@
+<?php
+	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$deviceid = $_POST['deviceid'];
+	
+	$query = mysql_query("SELECT * FROM ".logindb." WHERE ".logindb_user." = '$username' AND ".logindb_pass." = '$password'");
+	$num = mysql_num_rows($query);
+	
+	$validCharacters = "abcdefghijklmnopqrstuxyvwzABCDEFGHIJKLMNOPQRSTUXYVWZ0123456789";
+	$validCharNumber=strlen($validCharacters);
+	$length = 15;
+	$secretkey = "";
+	    
+		for ($i = 0; $i < $length; $i++) 
+		{
+            $index = mt_rand(0, $validCharNumber-1);
+            $secretkey .= $validCharacters[$index];
+        }
+	
+	if($num == 1)
+	{
+		$output = array('status' => $secretkey);
+		$query = mysql_query("UPDATE ".mobiledb." SET ".mobiledb_device." = '$deviceid' WHERE ".mobiledb_user." = '$username'");
+		$query = mysql_query("UPDATE ".mobiledb." SET ".mobiledb_key." = '$secretkey' WHERE ".mobiledb_user." = '$username'");
+	}
+	else
+	{
+		$output = array('status' => "dontmess");
+	}
+	echo json_encode($output);
+	mysql_close();
+?>	   
